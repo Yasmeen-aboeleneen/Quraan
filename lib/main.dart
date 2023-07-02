97aa0cc069eda538b49e4206541b92ab636c577e
Yasmeen Abo Eleneen <yasmeenaboeleneen3@gmail.com>
2023-07-02 20:34:20 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Quraan());
+}
+
+class Quraan extends StatelessWidget {
+  const Quraan({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quraan',
+      
+     
+    );
+  }
+}
+
+ 
+
+   
+
+  
+
+  
