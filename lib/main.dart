4b798ff62937054740ca1a82d85ac1499a70618c
Yasmeen Abo Eleneen <yasmeenaboeleneen3@gmail.com>
2023-07-11 04:55:10 +0300

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,13 @@ void main() {
 
 class Quraan extends StatelessWidget {
   const Quraan({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return  const MaterialApp(
+      
       title: 'Quraan',
       
+debugShowCheckedModeBanner: false,
      
     );
   }
